ffd802b0bb62a1af38d57205a5600b0ddb7d41f1
Ewan Keith <ewan.keith100@gmail.com>
2020-03-26 22:35:05 +0000

    touched up stream cnoverter with materialiser, refactored the generic stream converter to use this, and expanded unit testing for both

--- src/test/scala/org/ekeith/zio/akka/stream/ConvertersSpec.scala
+++ src/test/scala/org/ekeith/zio/akka/stream/ConvertersSpec.scala
@@ -6,8 +6,10 @@ import akka.stream.Materializer
 import akka.stream.scaladsl.{ Keep, RunnableGraph, Sink, Source => AkkaSource }
 import zio.Task
 import zio.test.{ assert, suite, testM, DefaultRunnableSpec, Spec, TestFailure, TestSuccess, ZSpec }
-import zio.test.Assertion.{ anything, equalTo, hasMessage, isLeft, isSubtype }
-import zio.test.environment.TestEnvironment
+import zio.test.Assertion.{ anything, equalTo, hasMessage, isLeft, isSubtype, isTrue }
+import zio.test.environment.{ Live, TestEnvironment }
+import zio.test.TestAspect._
+import zio.duration._
 
 import scala.concurrent.Future
 
@@ -15,10 +17,12 @@ object ConvertersSpec extends DefaultRunnableSpec {
 
   import Converters._
 
-  def spec: ZSpec[TestEnvironment, Any] = suite("All Tests")(
-    RunnableGraphAsTaskSuite,
-    AkkaSourceAsZioStreamSuite
-  )
+  def spec: ZSpec[TestEnvironment, Any] =
+    suite("All Tests")(
+      RunnableGraphAsTaskSuite,
+      AkkaSourceAsZioStreamSuite,
+      AkkaSourceAsZioStreamMSuite
+    ) @@ timed
 
   val RunnableGraphAsTaskSuite: Spec[Any, TestFailure[Throwable], TestSuccess] = suite("runnableGraphAsTaskSpec")(
     testM("Converted graph that sums a list of integers materialises the correct result") {
@@ -118,8 +122,8 @@ object ConvertersSpec extends DefaultRunnableSpec {
         actorSystem <- Task(ActorSystem("Test"))
         testSource  <- Task(AkkaSource(1 to 10))
         mat         <- Task(Materializer(actorSystem))
-        zioStream   = akkaSourceAsZioStream(testSource).provide(mat)
-        output      <- zioStream.map(_ * 2).fold(0)(_ + _)
+        zioStream   <- akkaSourceAsZioStream(testSource)
+        output      <- zioStream.map(_ * 2).fold(0)(_ + _).provide(mat)
         _           <- Task(actorSystem.terminate())
       } yield assert(output)(equalTo(110))
     },
@@ -128,9 +132,9 @@ object ConvertersSpec extends DefaultRunnableSpec {
         actorSystem <- Task(ActorSystem("Test"))
         testSource  <- Task(AkkaSource(1 to 10))
         mat         <- Task(Materializer(actorSystem))
-        zioStream   = akkaSourceAsZioStream(testSource).provide(mat)
-        output1     <- zioStream.fold(0)(_ + _)
-        output2     <- zioStream.fold(10)(_ + _)
+        zioStream   <- akkaSourceAsZioStream(testSource)
+        output1     <- zioStream.fold(0)(_ + _).provide(mat)
+        output2     <- zioStream.fold(10)(_ + _).provide(mat)
         output      = (output1, output2)
         _           <- Task(actorSystem.terminate())
       } yield assert(output)(equalTo((55, 65)))
@@ -141,8 +145,8 @@ object ConvertersSpec extends DefaultRunnableSpec {
         actorSystem <- Task(ActorSystem("Test"))
         testSource  <- Task(AkkaSource(-1 to 1).map(5 / _))
         mat         <- Task(Materializer(actorSystem))
-        zioStream   = akkaSourceAsZioStream(testSource).provide(mat)
-        output      <- zioStream.fold(0)(_ + _).either
+        zioStream   <- akkaSourceAsZioStream(testSource)
+        output      <- zioStream.fold(0)(_ + _).either.provide(mat)
         _           <- Task(actorSystem.terminate())
       } yield assert(output)(
         isSubtype[Either[ArithmeticException, Int]](anything) &&
@@ -169,8 +173,8 @@ object ConvertersSpec extends DefaultRunnableSpec {
       for {
         actorSystem <- Task(ActorSystem("Test"))
         mat         <- Task(Materializer(actorSystem))
-        zioStream   = akkaSourceAsZioStream(sideEffectingSource).provide(mat)
-        output      <- zioStream.fold(0)(_ + _)
+        zioStream   <- akkaSourceAsZioStream(sideEffectingSource)
+        output      <- zioStream.fold(0)(_ + _).provide(mat)
         _           <- Task(actorSystem.terminate())
       } yield assert(output)(equalTo(6)) &&
         assert(effectState)(equalTo(targetState))
@@ -192,8 +196,8 @@ object ConvertersSpec extends DefaultRunnableSpec {
       for {
         actorSystem <- Task(ActorSystem("Test"))
         mat         <- Task(Materializer(actorSystem))
-        zioStream   = akkaSourceAsZioStream(sideEffectingSource).provide(mat)
-        output      <- zioStream.fold(0)(_ + _).either
+        zioStream   <- akkaSourceAsZioStream(sideEffectingSource)
+        output      <- zioStream.fold(0)(_ + _).either.provide(mat)
         _           <- Task(actorSystem.terminate())
       } yield assert(output)(
         isSubtype[Either[ArithmeticException, Int]](anything) &&
@@ -224,11 +228,181 @@ object ConvertersSpec extends DefaultRunnableSpec {
       for {
         actorSystem <- Task(ActorSystem("Test"))
         mat         <- Task(Materializer(actorSystem))
-        _           = akkaSourceAsZioStream(sideEffectingSource1).provide(mat)
-        zioStream2  = akkaSourceAsZioStream(sideEffectingSource2).provide(mat)
-        _           <- zioStream2.fold(0)(_ + _)
+        _           <- akkaSourceAsZioStream(sideEffectingSource1)
+        zioStream2  <- akkaSourceAsZioStream(sideEffectingSource2)
+        _           <- zioStream2.fold(0)(_ + _).provide(mat)
         _           <- Task(actorSystem.terminate())
       } yield assert(effectState)(equalTo(targetState))
     }
   )
+
+  val AkkaSourceAsZioStreamMSuite: Spec[Live, TestFailure[Throwable], TestSuccess] =
+    suite("akkaSourceAsZioStreamMSpec")(
+      testM("Converted Akka source can be properly mapped and folded over as a ZIO Stream") {
+        for {
+          actorSystem    <- Task(ActorSystem("Test"))
+          testSource     <- Task(AkkaSource(1 to 10))
+          mat            <- Task(Materializer(actorSystem))
+          (zioStream, _) <- akkaSourceAsZioStreamM(testSource)
+          output         <- zioStream.map(_ * 2).fold(0)(_ + _).provide(mat)
+          _              <- Task(actorSystem.terminate())
+        } yield assert(output)(equalTo(110))
+      },
+      testM("Converted Akka source should be able to be evaluated as a ZStream multiple times") {
+        for {
+          actorSystem    <- Task(ActorSystem("Test"))
+          testSource     <- Task(AkkaSource(1 to 10))
+          mat            <- Task(Materializer(actorSystem))
+          (zioStream, _) <- akkaSourceAsZioStreamM(testSource)
+          output1        <- zioStream.fold(0)(_ + _).provide(mat)
+          output2        <- zioStream.fold(10)(_ + _).provide(mat)
+          output         = (output1, output2)
+          _              <- Task(actorSystem.terminate())
+        } yield assert(output)(equalTo((55, 65)))
+      },
+      testM("Source that throws an exception should be caught correctly by ZIO") {
+
+        for {
+          actorSystem    <- Task(ActorSystem("Test"))
+          testSource     <- Task(AkkaSource(-1 to 1).map(5 / _))
+          mat            <- Task(Materializer(actorSystem))
+          (zioStream, _) <- akkaSourceAsZioStreamM(testSource)
+          output         <- zioStream.fold(0)(_ + _).either.provide(mat)
+          _              <- Task(actorSystem.terminate())
+        } yield assert(output)(
+          isSubtype[Either[ArithmeticException, Int]](anything) &&
+            isLeft(hasMessage(equalTo("/ by zero")))
+        )
+      },
+      testM("A converted source evaluates basic side effects when ran") {
+        import scala.collection.mutable.{ Map => MMap }
+
+        val targetState = MMap(
+          1 -> "record for key 1",
+          2 -> "record for key 2",
+          3 -> "record for key 3"
+        )
+
+        val effectState = MMap[Int, String]()
+        val updateState: (Int, MMap[Int, String]) => Int =
+          (key: Int, state: MMap[Int, String]) => { state += (key -> s"record for key $key"); key }
+
+        val sideEffectingSource: AkkaSource[Int, NotUsed] =
+          AkkaSource(List(1, 2, 3))
+            .map(updateState(_, effectState))
+
+        for {
+          actorSystem    <- Task(ActorSystem("Test"))
+          mat            <- Task(Materializer(actorSystem))
+          (zioStream, _) <- akkaSourceAsZioStreamM(sideEffectingSource)
+          output         <- zioStream.fold(0)(_ + _).provide(mat)
+          _              <- Task(actorSystem.terminate())
+        } yield assert(output)(equalTo(6)) &&
+          assert(effectState)(equalTo(targetState))
+      },
+      testM("A converted source with side effects should not evaluate any effects past a thrown exception") {
+        import scala.collection.mutable.{ Map => MMap }
+
+        val targetState = MMap(-5 -> "record for key -5")
+
+        val effectState = MMap[Int, String]()
+        val updateState: (Int, MMap[Int, String]) => Int =
+          (key: Int, state: MMap[Int, String]) => { state += (key -> s"record for key $key"); key }
+
+        val sideEffectingSource: AkkaSource[Int, NotUsed] =
+          AkkaSource(-1 to 1)
+            .map(5 / _)
+            .map(updateState(_, effectState))
+
+        for {
+          actorSystem    <- Task(ActorSystem("Test"))
+          mat            <- Task(Materializer(actorSystem))
+          (zioStream, _) <- akkaSourceAsZioStreamM(sideEffectingSource)
+          output         <- zioStream.fold(0)(_ + _).either.provide(mat)
+          _              <- Task(actorSystem.terminate())
+        } yield assert(output)(
+          isSubtype[Either[ArithmeticException, Int]](anything) &&
+            isLeft(hasMessage(equalTo("/ by zero")))
+        ) && assert(effectState)(equalTo(targetState))
+      },
+      testM("A converted source doesn't evaluate its side effects if the ZStream is not completed (e.g. folded over)") {
+        import scala.collection.mutable.{ Map => MMap }
+
+        val targetState = MMap(
+          2 -> "record for key 2",
+          4 -> "record for key 4",
+          6 -> "record for key 6"
+        )
+
+        val effectState = MMap[Int, String]()
+        val updateState: (Int, MMap[Int, String]) => Int =
+          (key: Int, state: MMap[Int, String]) => { state += (key -> s"record for key $key"); key }
+
+        val sideEffectingSource1 =
+          AkkaSource(List(1, 2, 3))
+            .map(updateState(_, effectState))
+
+        val sideEffectingSource2 =
+          AkkaSource(List(2, 4, 6))
+            .map(updateState(_, effectState))
+
+        for {
+          actorSystem     <- Task(ActorSystem("Test"))
+          mat             <- Task(Materializer(actorSystem))
+          (_, _)          <- akkaSourceAsZioStreamM(sideEffectingSource1)
+          (zioStream2, _) <- akkaSourceAsZioStreamM(sideEffectingSource2)
+          _               <- zioStream2.fold(0)(_ + _).provide(mat)
+          _               <- Task(actorSystem.terminate())
+        } yield assert(effectState)(equalTo(targetState))
+      },
+      testM("Materialised value is produced after stream is ran") {
+        for {
+          actorSystem       <- Task(ActorSystem("Test"))
+          testSource        <- Task(AkkaSource(1 to 10).mapMaterializedValue(_ => 5))
+          mat               <- Task(Materializer(actorSystem))
+          (zioStream, m)    <- akkaSourceAsZioStreamM(testSource)
+          output            <- zioStream.map(_ * 2).fold(0)(_ + _).provide(mat)
+          materialisedValue <- m
+          _                 <- Task(actorSystem.terminate())
+        } yield assert(output)(equalTo(110)) &&
+          assert(materialisedValue)(equalTo(5))
+      },
+      testM("Materialised value is produced even if stream evaluation begins after materialised value evaluation") {
+        for {
+          actorSystem       <- Task(ActorSystem("Test"))
+          testSource        <- Task(AkkaSource(1 to 10).mapMaterializedValue(_ => 5))
+          mat               <- Task(Materializer(actorSystem))
+          (zioStream, m)    <- akkaSourceAsZioStreamM(testSource)
+          fibre             <- m.fork
+          output            <- zioStream.map(_ * 2).fold(0)(_ + _).provide(mat)
+          materialisedValue <- fibre.join
+          _                 <- Task(actorSystem.terminate())
+        } yield assert(output)(equalTo(110)) &&
+          assert(materialisedValue)(equalTo(5))
+      },
+      testM("Materialised value is never produced if the matching stream is not evaluated") {
+        for {
+          actorSystem <- Task(ActorSystem("Test"))
+          testSource  <- Task(AkkaSource(1 to 10).mapMaterializedValue(_ => 5))
+          (_, m)      <- akkaSourceAsZioStreamM(testSource)
+          _           <- m
+          _           <- Task(actorSystem.terminate())
+        } yield assert(false)(isTrue) // test will fail if completes
+      } @@ forked @@ nonTermination(5.seconds),
+      testM("Materialsied source value will be successfully evaluated even if the stream fails with an exception") {
+
+        for {
+          actorSystem       <- Task(ActorSystem("Test"))
+          testSource        <- Task(AkkaSource(-1 to 1).map(5 / _).mapMaterializedValue(_ => 5))
+          mat               <- Task(Materializer(actorSystem))
+          (zioStream, m)    <- akkaSourceAsZioStreamM(testSource)
+          output            <- zioStream.fold(0)(_ + _).either.provide(mat)
+          materialisedValue <- m
+          _                 <- Task(actorSystem.terminate())
+        } yield assert(output)(
+          isSubtype[Either[ArithmeticException, Int]](anything) &&
+            isLeft(hasMessage(equalTo("/ by zero")))
+        ) && assert(materialisedValue)(equalTo(5))
+      }
+    )
 }
